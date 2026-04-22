4ae799d2634a01918cb6e566e41f9b7cb0947c44
ViceAsd18 <vicen.ramirezduocuc.cl>
2026-04-22 04:32:12 -0400

    MS(region) crear request - agregarRegion y actualizarRegion

---
+++ region/src/main/java/com/satmed/region/models/request/ActualizarRegion.java
@@ -0,0 +1,11 @@
+package com.satmed.region.models.request;
+
+import lombok.Data;
+
+@Data
+public class ActualizarRegion {
+    
+    private Integer idRegion;
+    private String nombreRegion;
+
+}
