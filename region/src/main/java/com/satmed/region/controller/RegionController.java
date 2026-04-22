0d9c271ed8491e92543763f65158f3ce87c19740
ViceAsd18 <vicen.ramirezduocuc.cl>
2026-04-22 16:06:58 -0400

    Simplificar MS regiones

--- region/src/main/java/com/satmed/region/controller/RegionController.java
+++ region/src/main/java/com/satmed/region/controller/RegionController.java
@@ -3,23 +3,18 @@ package com.satmed.region.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.satmed.region.models.entities.Region;
-import com.satmed.region.models.request.ActualizarRegion;
-import com.satmed.region.models.request.AgregarRegion;
 import com.satmed.region.services.RegionService;
 
 @RestController
 @RequestMapping("regiones")
 public class RegionController {
+    
     
     @Autowired
     private RegionService regionService;
@@ -32,21 +27,6 @@ public class RegionController {
     @GetMapping("/{id}")
     public Region obtenerRegionPorId(@PathVariable Integer id) {
         return regionService.obtenerRegionPorId(id);
-    }
-
-    @PostMapping
-    public Region agregarRegion(@RequestBody AgregarRegion region) {
-        return regionService.agregarRegion(region);
-    }
-
-    @PutMapping
-    public Region actualizarRegion(@RequestBody ActualizarRegion region) {
-        return regionService.actualizarRegion(region.getIdRegion(), region);
-    }
-
-    @DeleteMapping("/{id}")
-    public String eliminarRegion(@PathVariable Integer id) {
-        return regionService.eliminarRegion(id);
     }
 
 }
--- region/src/main/java/com/satmed/region/services/RegionService.java
+++ region/src/main/java/com/satmed/region/services/RegionService.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.satmed.region.models.entities.Region;
-import com.satmed.region.models.request.ActualizarRegion;
-import com.satmed.region.models.request.AgregarRegion;
 import com.satmed.region.repositories.RegionRepository;
 
 @Service
@@ -22,9 +20,7 @@ public class RegionService {
         return regionRepository.findAll();
     }
 
-    
     public Region obtenerRegionPorId(Integer id) {
-
         Region regionEncontrada = regionRepository.findById(id).orElse(null);
 
         if (regionEncontrada == null) {
@@ -33,33 +29,4 @@ public class RegionService {
 
         return regionEncontrada;
     }
-
-    public Region agregarRegion(AgregarRegion region) {
-        Region nuevaRegion = new Region();
-        nuevaRegion.setNombreRegion(region.getNombreRegion());
-        return regionRepository.save(nuevaRegion);
-    }
-
-    public Region actualizarRegion(Integer id, ActualizarRegion region) {
-        Region regionExistente = regionRepository.findById(id).orElse(null);
-
-        if (regionExistente == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Región con ID: " + id + " no encontrada");
-        }
-
-        regionExistente.setNombreRegion(region.getNombreRegion());
-        return regionRepository.save(regionExistente);
-    }
-
-    public String eliminarRegion(Integer id) {
-        Region regionExistente = regionRepository.findById(id).orElse(null);
-
-        if (regionExistente == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Región con ID: " + id + " no encontrada");
-        }
-
-        regionRepository.delete(regionExistente);
-        return "Región eliminada correctamente";
-    }
-
 }
