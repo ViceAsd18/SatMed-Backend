a9d2b31b53624b8a30cead65503e39fc74e521cb
ViceAsd18 <vicen.ramirezg@duocuc.lc>
2026-04-23 09:27:07 -0400

    MS(alergia) subir service y controller

---
+++ alergias/src/main/java/com/satmed/alergias/controller/AlergiaController.java
@@ -0,0 +1,36 @@
+package com.satmed.alergias.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.satmed.alergias.models.entities.Alergia;
+import com.satmed.alergias.services.AlergiaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@RestController
+@RequestMapping("alergias")
+public class AlergiaController {
+    
+    @Autowired
+    private AlergiaService alergiaService;
+
+    @GetMapping("")
+    public List<Alergia> obtenerAlergias() {
+        return alergiaService.obtenerAlergias();
+    }
+
+    @GetMapping("/{id}")
+    public Alergia obtenerAlergiaPorId(@PathVariable Integer idAlergia) {
+        return alergiaService.obtenerAlergiaPorId(idAlergia);
+    }
+    
+    
+
+}
