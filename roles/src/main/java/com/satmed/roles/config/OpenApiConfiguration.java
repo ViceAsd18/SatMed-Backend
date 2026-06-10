40ae7d73403066b20922dea10986eaad4d03a1b1
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-06-10 16:54:46 -0400

    MS(Roles) Agregar swagger

---
+++ roles/src/main/java/com/satmed/roles/config/OpenApiConfiguration.java
@@ -0,0 +1,34 @@
+package com.satmed.roles.config;
+
+import io.swagger.v3.oas.models.info.Info;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+
+
+@Configuration
+public class OpenApiConfiguration {
+    
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Bean
+    public OpenAPI documentacionOpenApi(){
+        return new OpenAPI()
+            .info(new Info()
+            .title("Catalogo de Roles API")
+            .description("Consultar roles a utilizar en SAT-MED")
+            .version(appVersion)
+            .contact(new Contact()
+                .name("Vicente Ramírez - Lisett Granadillo")
+                .email("vicen.ramirezg@duocuc.cl - li.granadillo@duocuc.cl")
+            )
+        );
+    }
+
+}
