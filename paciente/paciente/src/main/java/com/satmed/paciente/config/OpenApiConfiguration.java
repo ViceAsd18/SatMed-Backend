004729400cf8506b2666ccb2e6beb6b361d93a48
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-06-11 14:14:40 -0400

    MS(Pacientes) implementar swagger

---
+++ paciente/paciente/src/main/java/com/satmed/paciente/config/OpenApiConfiguration.java
@@ -0,0 +1,33 @@
+package com.satmed.paciente.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+
+import io.swagger.v3.oas.models.info.Info;
+
+
+@Configuration
+public class OpenApiConfiguration {
+    
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Bean
+    public OpenAPI documentacionOpenApi(){
+        return new OpenAPI()
+            .info(new Info()
+            .title("Gestion de pacientes API")
+            .description("Consultar - Agregar - Editar - Eliminar pacientes a utilizar en SAT-MED")
+            .version(appVersion)
+            .contact(new Contact()
+                .name("Vicente Ramírez - Lisett Granadillo")
+                .email("vicen.ramirezg@duocuc.cl - li.granadillo@duocuc.cl")
+            )
+        );
+    }
+
+}
