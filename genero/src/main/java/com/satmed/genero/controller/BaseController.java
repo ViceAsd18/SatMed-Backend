aece978e739b86ed87d2c37f4efd24047c33fc5f
LisettGv <li.granadillo@duocuc.cl>
2026-06-13 18:16:14 -0400

    Sincronizar Swagger, versiones y controladores en Usuarios

---
+++ genero/src/main/java/com/satmed/genero/controller/BaseController.java
@@ -0,0 +1,29 @@
+package com.satmed.genero.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.satmed.genero.models.dto.VersionInfo;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Tag(name = "Base", description = "Controlador raíz para verificar el estado del microservicio")
+public class BaseController {
+    
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("")
+    @Operation(summary = "Obtener información de versión del microservicio")
+    public VersionInfo base() {
+        return new VersionInfo(appName, appVersion);
+    }
+}
