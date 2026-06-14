179375e3b0c9c9dc7d8b628d7c646e6044c034dd
LisettGv <li.granadillo@duocuc.cl>
2026-06-13 21:16:11 -0400

    Sincronizar config OpenApiConfiguration Paciente

---
+++ usuarios/src/main/java/com/satmed/usuarios/config/OpenApiConfiguration.java
@@ -0,0 +1,30 @@
+package com.satmed.usuarios.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfiguration {
+    
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Bean
+    public OpenAPI documentacionOpenApi(){
+        return new OpenAPI()
+            .info(new Info()
+            .title("Catálogo de Usuarios API")
+            .description("Consultar y gestionar usuarios a utilizar en SAT-MED")
+            .version(appVersion)
+            .contact(new Contact()
+                .name("Vicente Ramírez - Lisett Granadillo")
+                .email("vicen.ramirezg@duocuc.cl - li.granadillo@duocuc.cl")
+            )
+        );
+    }
+}
