71b57e27cc67af1274cda3ba162a29923e015d5a
ViceAsd18 <vicen.ramirezg@duocuc.cl>
2026-06-28 22:34:02 -0400

    MS(Estado Tratamiento) subir BaseController y swagger

---
+++ estadoTratamiento/src/main/java/com/satmed/estadoTratamiento/controller/BaseController.java
@@ -0,0 +1,29 @@
+package com.satmed.estadoTratamiento.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.satmed.estadoTratamiento.models.dto.VersionInfo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/")
+public class BaseController {
+    
+    @Value("${app.name}")
+    private String nombre;
+
+    @Value("${app.version}")
+    private String version;
+
+    @GetMapping("")
+    public VersionInfo base() {
+        return new VersionInfo(nombre, version);
+    }
+    
+
+}
